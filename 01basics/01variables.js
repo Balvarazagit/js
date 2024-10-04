52bc1f07beb3a226a8330bb8ea9b182c01d11573
Balva Aliraza Roshanali <211260116007setiit@gmail.com>
2024-10-04 06:30:10 +0000

    variables done

---
+++ 01basics/01variables.js
@@ -0,0 +1,14 @@
+const accountid = 1234;
+let accountname = "Balva";
+var accountemail = "hell@gmail.com";
+accountcity = "ahmedabad";
+let accountstate;
+
+// accountid = 3232;
+accountname ="aliraza";
+accountemail ="ali@gmail.com";
+accountcity ="jaipur";
+
+
+// console.log(accountid,accountemail,accountname,accountcity);
+console.table([accountid,accountname,accountemail,accountcity,accountstate]);

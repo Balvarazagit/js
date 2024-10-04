39ee2c59ee1054fceeacfab492bd97637e2ac9d4
Balva Aliraza Roshanali <211260116007setiit@gmail.com>
2024-10-04 06:59:58 +0000

    datatypes

---
+++ 01basics/02datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as a newer versions of js.
+
+// alert(3+3) // we are using the node js not use browser then not run in terminal.
+
+// Primitive datatypes
+
+// number => 112
+// string => ""
+// boolean => true OR false
+// bigint
+// null =>standoloan value
+// undefined =>
+// symbol => unique
+
+
+// non primitive datatypes
+
+// object
+// array
+
+console.log(typeof null) // type is object
+console.log(typeof undefined) // type is undefined

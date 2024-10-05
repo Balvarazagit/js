6ff9b3e4ad677408d013c42e207059ffe0a69d57
Balva Aliraza Roshanali <211260116007setiit@gmail.com>
2024-10-05 14:52:33 +0000

    numandmath

---
+++ 01basics/06numandmath.js
@@ -0,0 +1,39 @@
+// Number
+const num = new Number(100);
+
+// console.log(num);
+
+// console.log(num.toString().length);
+// console.log(num.toFixed(3));
+
+const num1 = 123333344
+// console.log(num1.toPrecision(3));
+
+// console.log(num1.toLocaleString('en-In'));
+
+
+// // math
+// console.log(Math.abs(-3));
+// console.log(Math.sqrt(25));
+// console.log(Math.round(2.67));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.max(3,2,9,4));
+// console.log(Math.min(3,9,2,1));
+
+
+// console.log(Math.random());
+// console.log((Math.random() * 10) + 1);
+// console.log(Math.floor((Math.random() * 10) + 1));
+
+const min = 1;
+const max = 6;
+
+console.log(Math.floor((Math.random() * (max - min + 1)) + min));
+console.log(Math.floor((Math.random() * (max - min + 1)) + min));
+console.log(Math.floor((Math.random() * (max - min + 1)) + min));
+console.log(Math.floor((Math.random() * (max - min + 1)) + min));
+console.log(Math.floor((Math.random() * (max - min + 1)) + min));
+console.log(Math.floor((Math.random() * (max - min + 1)) + min));
+console.log(Math.floor((Math.random() * (max - min + 1)) + min));
+

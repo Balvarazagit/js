f232c843f37e96a6d7d5da85fbdaf0a0cb349e8b
Balva Aliraza Roshanali <211260116007setiit@gmail.com>
2024-10-10 10:39:10 +0000

    function

---
+++ 03basics/01functions.js
@@ -0,0 +1,49 @@
+// function saymyname () {
+//     console.log("a");
+//     console.log("l");
+//     console.log("i");
+// }
+
+// // saymyname()
+
+// function twonumberadd(n1,n2){
+//     if((typeof n1) == 'number'){
+//         if((typeof n2 == 'number')){
+//         console.log(n1 + n2);
+//     }
+//     else{
+//         console.log("write number!");
+        
+//     }
+// }
+//     else{
+//         console.log("write number!");
+        
+//     }
+// // console.log(n1 + n2);
+// }
+
+// twonumberadd(4,4)
+
+function add(n1,n2){
+    // let result = n1+n2;
+    // return result
+    return n1+n2;
+}
+const result = add(4,5);
+// console.log(result);
+
+function name(uname = "abcd"){
+    if(!uname){
+        console.log("please...");
+        return
+    }
+
+    return `${uname} logged`
+}
+
+// console.log(name("ali"));
+console.log(name());// output : undefined logged
+console.log(name("gfds"));
+
+

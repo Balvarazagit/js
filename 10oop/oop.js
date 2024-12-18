63cc6298fd375e2da9ab85010a0b59b8a71a981b
Balva Aliraza Roshanali <211260116007setiit@gmail.com>
2024-12-18 12:31:22 +0000

    oop

--- 10oop/oop.js
+++ 10oop/oop.js
@@ -1,3 +1,5 @@
+//object literals
+
 const user = {
     usernaame : 'ali',
     logincount : 4,
@@ -5,12 +7,33 @@ const user = {
 
     getusers:function(){
       //  console.log('user detailed from databases');
-      console.log(`username : ${this.usernaame}`);
-      
-        
+      // console.log(`username : ${this.usernaame}`);
+      // console.log(this);
     }
 }
 
-console.log(user.logincount);
-console.log(user.getusers());
+// console.log(user.logincount);
+// console.log(user.getusers());
+// console.log(this);
+
+
+// constructor function
+function user1(username,login,logout){
+  this.username = username;
+  this.login = login;
+  this.logout = logout;
+
+  this.greet = function(){
+    console.log(`ali : ${this.username}`);
+    
+  }
+  //  return this;
+}
+// console.log(user1("ali",true,false));
+const user2 = new user1("balva",true,true)
+// console.log(user2);
+console.log(user1.constructor);
+
+
+
 

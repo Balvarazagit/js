bc56b2aa0e22498703b517735fd1504cab01ccd7
Balva Aliraza Roshanali <211260116007setiit@gmail.com>
2024-12-20 15:11:15 +0000

    tic

--- 07projects/7.tictactoe/script.js
+++ 07projects/7.tictactoe/script.js
@@ -1,7 +1,18 @@
 let currentplayer = "X";
 let arr = Array(9).fill(null);
 
-
+function checkwinner(){
+    if(
+        (arr[0]==arr[1] && arr[1]==arr[2]) || 
+        (arr[3]==arr[4] && arr[4]==arr[5]) ||
+        (arr[6]==arr[7] && arr[7]==arr[8]) ||
+        (arr[0]==arr[4] && arr[4]==arr[8]) ||
+        (arr[2]==arr[4] && arr[4]==arr[6]) 
+    ){
+        console.log(currentplayer);
+        
+    }       
+}
 
 function handleclick(e){
     const id = Number(e.id);

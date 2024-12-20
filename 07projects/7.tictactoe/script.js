db4f1192e48c2a74a6f56db0750b24d35a5e2ee1
Balva Aliraza Roshanali <211260116007setiit@gmail.com>
2024-12-20 15:15:34 +0000

    tic

--- 07projects/7.tictactoe/script.js
+++ 07projects/7.tictactoe/script.js
@@ -3,11 +3,11 @@ let arr = Array(9).fill(null);
 
 function checkwinner(){
     if(
-        (arr[0]==arr[1] && arr[1]==arr[2]) || 
-        (arr[3]==arr[4] && arr[4]==arr[5]) ||
-        (arr[6]==arr[7] && arr[7]==arr[8]) ||
-        (arr[0]==arr[4] && arr[4]==arr[8]) ||
-        (arr[2]==arr[4] && arr[4]==arr[6]) 
+        (arr[0] !== arr[0]==arr[1] && arr[1]==arr[2]) || 
+        (arr[3] !== arr[3]==arr[4] && arr[4]==arr[5]) ||
+        (arr[6] !== arr[6]==arr[7] && arr[7]==arr[8]) ||
+        (arr[0] !== arr[0]==arr[4] && arr[4]==arr[8]) ||
+        (arr[2] !== arr[2]==arr[4] && arr[4]==arr[6]) 
     ){
         console.log(currentplayer);
         

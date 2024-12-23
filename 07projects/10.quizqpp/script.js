ecdec5c3a14f56ae5e3262207f1c2690211b0183
Balva Aliraza Roshanali <211260116007setiit@gmail.com>
2024-12-23 06:05:06 +0000

    quiz

--- 07projects/10.quizqpp/script.js
+++ 07projects/10.quizqpp/script.js
@@ -23,13 +23,71 @@ const question = [
     'd':'prophet musa a.s.',
     'correct': 'd'
 },
-
 ]
+
 let index =0;
-const quesbox = document.getElementById('quesbox')
+let total = question.length;
+let right = 0, wrong = 0; 
+const quesbox = document.getElementById('quesbox'); 
+const optioninput = document.querySelectorAll('.options');
+
 const loadquestion = () => {
+    if(index === total){
+       return endquiz();
+    }
+    else{
+        reset();
+    }
     const data = question[index];
     quesbox.innerHTML = `${index + 1}) ${data.que}`;
-    
+    optioninput[0].nextElementSibling.innerHTML = data.a; 
+    optioninput[1].nextElementSibling.innerHTML = data.b;
+    optioninput[2].nextElementSibling.innerHTML = data.c;
+    optioninput[3].nextElementSibling.innerHTML = data.d;
+}
+
+
+
+document.querySelector('.btn').addEventListener('click', 
+    submitquiz = ()=>{
+    const data = question[index];
+    const ans = getanswer();
+    if(ans == data.correct){
+        right++;
+    }
+    else{
+        wrong++;
+    }
+    index++;
+    loadquestion();
+    return;
+}
+)
+
+const getanswer = ()=>{
+    let answer;
+    optioninput.forEach(
+       (input)=>{
+        if(input.checked){
+        answer = input.value;  
+        }
+       }
+    )
+    return answer;
+}   
+
+const reset = ()=>{
+    optioninput.forEach(
+        (input)=>{
+         input.checked = false;
+        }
+     )
+}
+
+const endquiz = ()=>{
+    document.getElementById('box').innerHTML = `
+    <h3>Thank you playing the quiz</h3>
+    <h2>${right} and ${wrong}</h2>
+    `
 }
 loadquestion()

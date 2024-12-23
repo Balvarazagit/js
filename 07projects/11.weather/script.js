af2c0c08d1fce55d07a8daa85289278e8c999f63
Balva Aliraza Roshanali <211260116007setiit@gmail.com>
2024-12-23 10:02:21 +0000

    weather

--- 07projects/11.weather/script.js
+++ 07projects/11.weather/script.js
@@ -1,10 +1,22 @@
 const apikey = "808595b14ca664826a464948c2c18e64";
-const apiurl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=india";
+const apiurl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchbox = querySelector('.search input');
+const serachbtn = querySelector('.search button')
 
-async function checkapi (){
-    const response = await fetch(apiurl + `&appid=${apikey}`)
-    const data = response.json();
-    console.log(data);
+async function checkweather (city){
+    const response = await fetch(apiurl + city + `&appid=${apikey}`);
+    const data = await response.json();
     
+    document.querySelector('.city').innerHTML = data.name;
+    document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector('.humidity').innerHTML = data.main.humidity + "%";
+    document.querySelector('.wind').innerHTML = data.wind.speed + " KM/H";
+
 }
-checkapi()
+serachbtn.addEventListener('click', ()=>{
+    console.log(searchbox.value);
+    
+    checkweather(searchbox.value);
+})
+
+

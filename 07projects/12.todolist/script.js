d969363b9a7cc5d4e0b05cc9eea460e40c6add33
Balva Aliraza Roshanali <211260116007setiit@gmail.com>
2024-12-26 11:02:47 +0000

    todolist

--- 07projects/12.todolist/script.js
+++ 07projects/12.todolist/script.js
@@ -1,6 +1,6 @@
-const inputbox = document.querySelector('.input-box');
-const listcontainer = document.querySelector('.list-container');
-const button = document.querySelector('.row button');
+const inputbox = document.querySelector('#input-box'); //input field
+const listcontainer = document.querySelector('#list-container'); //ul
+const button = document.querySelector('.row button'); // button
 button.addEventListener('click',()=>{
     if(inputbox.value === ''){
         alert("You Must Write Somethings")
@@ -8,5 +8,30 @@ button.addEventListener('click',()=>{
     else{
         let li = document.createElement('li');
         li.innerHTML = inputbox.value;
+        listcontainer.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
+    // inputbox.value ="";
+    savedata();
 })
+listcontainer.addEventListener('click',(e)=>{
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        savedata()
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        savedata()
+    }
+},false)
+
+function savedata(){
+    localStorage.setItem("data",listcontainer.innerHTML);
+}
+
+function showtask(){
+    listcontainer.innerHTML = localStorage.getItem("data")
+}
+showtask(); 
